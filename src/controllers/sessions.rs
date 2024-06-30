0c3941e3cf7176676ebfe36e2da98c999808c931
Jordan Gomes <me@jordangomes.com>
2024-06-30 19:49:26 +1000

    shorten graph timespan

--- src/controllers/sessions.rs
+++ src/controllers/sessions.rs
@@ -9,7 +9,7 @@ use tide_sqlx::SQLxRequestExt;
 pub async fn auth_by_minute(req: Request<State>) -> tide::Result<Body> {
     let mut db_pool = req.sqlx_conn::<MySql>().await;
     let connection = db_pool.acquire().await?;
-    let rows = handlers::sessions::recent_auth_attempts(connection, 48).await?;
+    let rows = handlers::sessions::recent_auth_attempts(connection, 6).await?;
 
     Body::from_json(&rows)
 }
